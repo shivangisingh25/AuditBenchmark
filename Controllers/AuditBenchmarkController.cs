716679f1e940b9fed50805fdb85ef3ae03b0f520
AuditProjectMFRP <projectaudit2020@gmail.com>
2020-08-07 11:25:55 +0530

    Add project files.

---
+++ Controllers/AuditBenchmarkController.cs
@@ -0,0 +1,35 @@
+﻿using AuditBenchmark.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AuditBenchmark.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuditBenchmarkController : ControllerBase
+    {
+        readonly log4net.ILog _log4net;
+
+        public AuditBenchmarkController()
+        {
+            _log4net = log4net.LogManager.GetLogger(typeof(AuditBenchmarkController));
+        }
+
+        [HttpGet]
+        public ActionResult<List<AuditBenchmarkModel>> GetAuditBenchmark()
+        {
+            _log4net.Info(" Http GET request");
+
+            List<AuditBenchmarkModel> benchmark = new List<AuditBenchmarkModel>
+                    {
+                       new AuditBenchmarkModel{AuditType="Internal",BenchmarkNoAnswers=3},
+                       new AuditBenchmarkModel{AuditType="SOX",BenchmarkNoAnswers=1}
+                    };
+            return Ok(benchmark);
+
+
+
+        }
+
+    }
+}
---
+++ Model/AuditBenchmarkModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditBenchmark.Model
+{
+    public class AuditBenchmarkModel
+    {
+        public string AuditType { get; set; }
+        public int BenchmarkNoAnswers { get; set; }
+    }
+}
